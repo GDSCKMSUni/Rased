b97e41399cb2e27e848b87a1f5fa952826b8c7b7
Marwan M. Al-Falah <marwanmalfalah@gmail.com>
2023-03-25 19:16:00 -0700

    update pages

---
+++ lib/pages/rased_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/textfield_widget.dart';
+
+class RasedPage extends StatelessWidget {
+  const RasedPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF42C3A7),
+        title: const Text(
+          'أنت الراصد',
+          style: TextStyle(color: Color(0xFFFFFFFF)),
+        ),
+        centerTitle: true,
+      ),
+      body: Directionality(
+        textDirection: TextDirection.rtl,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const textFieldWidget(hint: 'الاسم',),
+            const textFieldWidget(hint: 'التاريخ'),
+            const textFieldWidget(hint: 'العنوان الرئيسي'),
+            const textFieldWidget(hint: 'رقم الهاتف (اختياري)'),
+            const textFieldWidget(
+              hint: 'تفاصيل اكثر',
+              maxlines: 5,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(onPressed: () {}, icon: Icon(Icons.mic)),
+                IconButton(
+                    onPressed: () {}, icon: Icon(Icons.video_call_rounded)),
+                IconButton(onPressed: () {}, icon: Icon(Icons.camera)),
+              ],
+            )
+          ],
+        ),
+
+      ),
+    );
+  }
+}
---
+++ lib/splah_page.dart
@@ -0,0 +1,73 @@
+import 'package:rasedapp_ye/pages/get_started.dart';
+
+import '../main.dart';
+import 'package:helpers/helpers.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart' hide Trans;
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:localize_and_translate/localize_and_translate.dart';
+
+import 'login/login_page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  goToLogin() {
+    if (auth.currentUser != null) {
+      Future.delayed(Duration(seconds: 5), () => Get.offAll(() => GetStarted()));
+    } else {
+      Future.delayed(Duration(seconds: 5), () => Get.offAll(() => LoginPage()));
+    }
+  }
+
+  @override
+  void initState() {
+    goToLogin();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            children: [
+              // Headline3(
+              //   "companyName".tr().replaceAll(" ", "\n"),
+              //   textAlign: TextAlign.center,
+              //   style:
+              //       TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+              // ),
+              SizedBox(height:50,),
+
+              Image.asset("assets/images/logo.png", width: 150, height: 150,),
+              SizedBox(height: 30,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                Text("جميع الحقوق محفوظة لدى Rased"),
+
+              ],)
+
+
+            ],
+
+          ),
+        ),
+      ),
+    );
+  }
+}
